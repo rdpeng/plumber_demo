50f611a4fb45eb7dbd0d3eacfd207e56f3ff6b95
Roger D. Peng <rdpeng@gmail.com>
2025-11-11 07:34:50 -0600

    More log info

--- test3.R
+++ test3.R
@@ -7,14 +7,15 @@ dataset <- read.csv("data.csv")
 #* Get Variable Names
 #* @get /variables
 function() {
-    message("getting names ", Sys.time())
+    message("getting names at ", Sys.time())
     names(dataset)
 }
 
 #* Get Variable Information
 #* @get /variableInfo/<varname>
 function(varname) {
-    message("getting variable info ", Sys.time())
+    message(sprintf("getting variable info for '%s' at %s",
+                    varname, format(Sys.time())))
     varname <- as.character(varname)
     if(!(varname %in% names(dataset))) {
         abort_bad_request(sprintf("varname '%s' not in dataset", varname))
