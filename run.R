446c60c864e70a2fabc7a902c3578472e9662375
Roger D. Peng <rdpeng@gmail.com>
2025-11-11 07:31:49 -0600

    Allow file input

--- run.R
+++ run.R
@@ -2,7 +2,17 @@
 
 library(plumber2)
 
-pa <- api("test2.R",
+args <- commandArgs(TRUE)
+rfile <- args
+if(length(rfile) > 1L) {
+    stop("only one file please!")
+}
+if(!file.exists(rfile)) {
+    stop("can't find file ", rfile)
+}
+
+pa <- api(rfile,
           host = "164.90.154.116",
-          port = 8000)
+          port = 8080)
 api_run(pa)
+
